db2c31744de52dcc84c7806fe3211f7f6e39896c
HyungBin <jhbfeel@naver.com>
2018-10-11 20:34:32 +0900

    commit

--- deliveryProject/src/main/webapp/resources/js/js1.js
+++ deliveryProject/src/main/webapp/resources/js/js1.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 	
-	$("#login").on("click",function(){
+/*	$("#login").on("click",function(){
 		alert("이야~ 최고다~");
 		console.log("이야~ 최고다~");
-	});
+	});*/
 	 
 });
 
